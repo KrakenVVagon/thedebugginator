6664daa2127f47b4af49f93551c8c1302397219c
KrakenVVagon <73725558+KrakenVVagon@users.noreply.github.com>
2022-01-09 16:11:18 -0500

    Updated project structure
    added placeholder files for all directories

---
+++ src/data/make_debugginator_dataset.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+'''
+Script to extract and preprocess data to be used in a debugginator model
+
+Created by: Andrew Younger
+2022-01-09
+'''
---
+++ src/models/train_debugginator.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+'''
+Source code for training thedebugginator on a dataset and saving the trained model.
+Training views and visualizations made from /../visualization/debugginator_viz_scripts.py
+
+Created by: Andrew Younger
+2022-01-09
+'''
