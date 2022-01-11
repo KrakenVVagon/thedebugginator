2522db303cd06889499999eaa59c65c285a61eea
KrakenVVagon <73725558+KrakenVVagon@users.noreply.github.com>
2022-01-11 13:52:43 -0500

    update datafiles

---
+++ debugginator/data/extractor.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+'''
+Classes and functions to extract or read data for preprocessing
+
+Created by: Andrew Younger
+2022-01-09
+'''
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers, losses
+from tensorflow.keras.models import Model
+
+class Extractor():
+    '''
+    Extractor class takes a file or path or database link and extracts desired feature columns.
+    
+    type: the type of extraction to do (file, path, database)
+    pyspark: if the extraction process is handled as pyspark functionality. defaults to false
+    '''
+    def __init__(self):
+        return None
+    
+class 
+    
+def main():
+    print('Extractor main loop')
+
+if __name__ == '__main__':
+    main()
--- debugginator/data/preprocesser.py
+++ debugginator/data/preprocesser.py
@@ -5,3 +5,21 @@ Script to extract and preprocess data to be used in a debugginator model
 Created by: Andrew Younger
 2022-01-09
 '''
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers, losses
+from tensorflow.keras.models import Model
+
+class Preprocesser():
+    '''
+    
+    '''
+    def __init__(self):
+        return None
+
+def main():
+    print('Preprocesser main loop')
+
+if __name__ == '__main__':
+    main()
