f89818d31239bd3ac0fb530185c8eb54db2998ec
KrakenVVagon <73725558+KrakenVVagon@users.noreply.github.com>
2022-01-10 20:31:10 -0500

    Created setup.py and __init__.pys
    Added setup.py
    fixed project structures
    project now pip installable

--- debugginator/__init__.py
+++ debugginator/__init__.py
@@ -5,3 +5,7 @@ Script to extract and preprocess data to be used in a debugginator model
 Created by: Andrew Younger
 2022-01-10
 '''
+
+__version__ = "0.1.0"
+__author__ = 'Andrew Younger'
+__credits__ = 'Ubisoft Toronto'
--- setup.py
+++ setup.py
@@ -5,3 +5,24 @@ Script to extract and preprocess data to be used in a debugginator model
 Created by: Andrew Younger
 2022-01-10
 '''
+
+from setuptools import setup, find_packages
+
+with open('src/__init__.py') as f:
+    info = {}
+    for line in f.readlines():
+        if line.startswith('__version__'):
+            exec(line,info)
+            break
+
+setup(
+    name='debugginator',
+    version=info['__version__'],
+    author='Andrew Younger',
+    packages=['src'] + ['src.'+pkg for pkg in find_packages('src')],
+    install_requires=['tensorflow>=2.7.0',
+                      'pandas>=1.3.4',
+                      'numpy>=1.21.4',
+                      'scikit-learn>=1.0.1'
+                     ]
+)
