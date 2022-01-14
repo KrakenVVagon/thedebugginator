01ca048ebe1c75bf05004d041709e7e81513f669
KrakenVVagon <73725558+KrakenVVagon@users.noreply.github.com>
2022-01-14 18:18:42 -0500

    Update preprocesser.py

--- debugginator/data/preprocesser.py
+++ debugginator/data/preprocesser.py
@@ -13,6 +13,7 @@ from tensorflow.keras.models import Model
 class Preprocesser():
     '''
     Preprocesser class takes a pandas dataframe as input and additional arguments to specify the encoding needed for the column
+    Used like a general model except done on a specific dataframe not a general object
 
     df : pandas dataframe input argument
     categorical_features : optional arugment to specify which columns should be vectorized. defaults to empty list
@@ -31,10 +32,46 @@ class Preprocesser():
         if len(self.numerical_features) == 0:
             self.numerical_features = [c for c in self.df.columns if self.df[c].dtype in ['int64','float64']]
 
-    def normalize(self,x):
-        return None
+    def normalize(self):
+        '''
+        Normalizes the numerical features of the dataframe. Resets the normalization attributes to empty.
+        '''
+        self.normalized_features = []
+        self.numerical_inputs = []
+        for n in self.numerical_features:
+            ninput = layers.Input(shape=(1,),dtype=tf.float32)
+            normalizer = layers.Normalization()
+            normalizer.adapt(self.df[n])
+            normalized_data = normalizer(ninput)
+            self.numerical_inputs.append(ninput)
+            self.normalized_features.append(normalized_data)
 
-    def encode(self,x):
+        print(f'Normalized {len(self.numerical_features)} of {len(self.df.columns)} total columns')
+
+    def encode(self):
+        '''
+        Encodes the categorical features of the dataframe. Resets the encoding attributes to empty
+        Currently only supports one-hot encoding of features
+        '''
+        self.categorical_inputs = []
+        self.encoded_features = []
+        for c in self.categorical_features:
+            cinput = layers.Input(shape=(1,),dtype=tf.string)
+            encoder = layers.StringLookup(output_mode='one_hot')
+            encoder.adapt(self.df[c].astype(str))
+            encoded = encoder(cinput)
+            self.categorical_inputs.append(cinput)
+            self.encoded_features.append(encoded)
+
+        print(f'Encoded {len(self.categorical_features)} of {len(self.df.columns)} total columns')
+
+    def train(self):
+        '''
+        Train the preprocesser to accept inputs in the same form as its own df
+        Assigns a model to the preprocesser that can be used to predict things
+        '''
+
+    def predict(self):
         return None
 
 def main():
@@ -44,8 +81,11 @@ def main():
         })
 
     pp = Preprocesser(x)
-    print(pp.categorical_features)
-    print(pp.numerical_features)
+    pp.normalize()
+    pp.encode()
+
+    print(pp.encoded_features)
+    print(pp.normalized_features)
 
 if __name__ == '__main__':
     main()
