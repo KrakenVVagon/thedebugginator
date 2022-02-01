f6aad2940732927b65f7234b32e301246b850e56
KrakenVVagon <73725558+KrakenVVagon@users.noreply.github.com>
2022-02-01 18:22:19 -0500

    Update autoencoder.py

--- debugginator/models/autoencoder.py
+++ debugginator/models/autoencoder.py
@@ -9,18 +9,52 @@ import tensorflow as tf
 from tensorflow.keras import layers, losses, Sequential
 from tensorflow.keras.models import Model
 
-class AnomalyDetector(Model):
-    def __init__(self):
-        super(AnomalyDetector,self).__init__()
-        self.encoder = Sequential([
+class Encoder():
+    '''
+    Encoder should take the initial size and the depth
 
-            ])
+    TODO: add logic to get good condensed size. This is all the heavy lifting for the autoencoder
+    '''
+    def __init__(self,initial_size,depth=1):
+        self.output_size=7
+        return None
 
-        self.decoder = Sequential([
+class Decoder():
+    '''
+    Decoder should take the output size (usually equal to the initial size), the condensed size, and the depth
+    '''
+    def __init__(self,output_size,encoded_size,depth=1):
+        return None
 
-            ])
+class AutoEncoder(Model):
+    '''
+    Basic class for an anomaly detector using an autoencoder
+    Should take the initial dimensions and the depth of learning as parameters
+    '''
+    def __init__(self,initial_size,encoder_depth=1):
+        super(AutoEncoder,self).__init__()
+        self.initial_size = initial_size
+        self.encoder_depth = encoder_depth
+
+        self.encoder = Encoder(self.initial_size,depth=self.encoder_depth)
+        self.latent_size = self.encoder.output_size
+        self.decoder = Decoder(self.initial_size,self.latent_size,depth=self.encoder_depth)
 
     def call(self,x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
+
+    def get_config(self):
+        return {
+                'initial_size':self.initial_size,
+                'encoder_depth':self.encoder_depth,
+                'latent_size':self.latent_size
+                }
+
+def main():
+    a = AutoEncoder(6)
+    print(a.get_config())
+
+if __name__ == '__main__':
+    main()
